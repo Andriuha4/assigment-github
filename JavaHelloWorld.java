c7cd2843d35a8dec9118b654300168d09f738393
Andriuha4 <120526211+Andriuha4@users.noreply.github.com>
2022-12-15 18:45:19 +0200

    Add files via upload

---
+++ JavaHelloWorld.java
@@ -0,0 +1,21 @@
+public class JavaProgram 
+{
+     
+       public static void main(String [] args)
+        {
+          for(int i=0;i<36;i++){
+
+          System.out.println("Hello World");
+          } 
+
+
+        }  
+
+}
+
+
+
+
+
+   
+
